6aecc1aac4d9d4b5baa1476cf6171074b7d47857
VIKAS BALANI <vblani@VIKASs-MBP.BWORLD>
2015-05-06 22:01:41 -0400

    v.0.13 Search split drop down added o nav bar

--- app/app.js
+++ app/app.js
@@ -148,3 +148,28 @@ app.controller('TypeaheadCtrl',['$scope','$http',  function($scope, $http) {
 } 
 ]
                                 );
+
+app.controller('DropdownCtrl', ['$scope','$log',function ($scope, $log) {
+  $scope.items = [
+    'The first choice!',
+    'And another choice for you.',
+    'but wait! A third!',
+      '4th',
+      'fifth',
+      'of course it\'s sixth'
+  ];
+
+  $scope.status = {
+    isopen: false
+  };
+
+  $scope.toggled = function(open) {
+    $log.log('Dropdown is now: ', open);
+  };
+
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+  };
+}]);
